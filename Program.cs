7df885926e9ac595e4ebea0da64edf1fc3695107
oederson <eder.hu@live.com.pt>
2023-09-28 05:50:50 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
-﻿using DesafioPOO.Models;
+﻿﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone nokia = new Nokia("1234", "Modelo 1", "1234567890ABCDEFGH", 64);
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Testando o smartphone Nokia");
+Console.ResetColor();
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Smartphone iphone = new Iphone("4321", "Modelo 2", "1234567890ABCDEFGH", 32);
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Testando o smartphone Iphone");
+Console.ResetColor();
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
